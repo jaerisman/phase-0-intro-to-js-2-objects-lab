c671fee2988fb6bd6ccf2453cf1754989f33e7ec
Jacob Erisman <jacob.erisman@gmail.com>
2023-08-14 18:52:08 -0400

    Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Jacob",
+    streetAddress: "Fernwood Ct."
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
