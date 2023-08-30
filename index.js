77f0340ea29fcf67602b9f2f349766c8d8c336b4
Jacob Erisman <jacob.erisman@gmail.com>
2023-08-30 15:02:43 -0400

    Lab Completed = JE

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const employee = {
-    name: "Jacob",
-    streetAddress: "Fernwood Ct."
+  name: 'Jacob',
+  streetAddress: 'Ferndwood Ct'
 }
-function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
   return {
     ...employee,
     [key]: value,
-  };
+  }
 }
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
   employee[key] = value;
-  return employee;
+  return employee
 }
+
 function deleteFromEmployeeByKey(employee, key, value){
-    return {
-        ...employee,
-        [key]: value,
-    }
+  return {
+    ...employee,
+    [key]: value,
+  }
 }
+
 function destructivelyDeleteFromEmployeeByKey(employee, key, value){
-    employee[key] = value;
-    return employee;
+  employee[key] = value;
+  return employee
 }
